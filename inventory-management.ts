ef30631f9d0a9eded79529c527632aa98bfcdc9c
Matthew Houston <matthewhouston1@gmail.com>
2025-08-25 21:43:31 +0200

    establish variables

---
+++ inventory-management.ts
@@ -0,0 +1,9 @@
+import { InventoryManager } from './inventory-manager';
+import { FileReader } from './file-reader';
+
+//Main function to run the program
+async function main() {
+    const inventoryFile = 'inventory.csv';
+    const supplierFile = 'supplierQuality.csv';
+    const currentDate = new Date('2024-06-10');
+}
