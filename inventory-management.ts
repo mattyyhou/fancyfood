a1a351754e87d17ec281ecd8b6c382e260179ee6
Matthew Houston <matthewhouston1@gmail.com>
2025-08-25 21:48:40 +0200

    file reading

--- inventory-management.ts
+++ inventory-management.ts
@@ -6,4 +6,9 @@ async function main() {
     const inventoryFile = 'inventory.csv';
     const supplierFile = 'supplierQuality.csv';
     const currentDate = new Date('2024-06-10');
+
+    const FileReader = new FileReader();
+    const inventoryData = await fileReader.readCSV(inventoryFile);
+    const supplierData = await FileReader.readCSV(supplierFile);
+
 }
