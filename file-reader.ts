63a07dd8e0a5aa4d2826b7bd4f7c157625087cc9
Matthew Houston <matthewhouston1@gmail.com>
2025-08-27 19:36:51 +0200

    define the function and how it will provide an array of results

--- file-reader.ts
+++ file-reader.ts
@@ -2,6 +2,12 @@ import { readFileSync } from 'fs';
 
 export class FileReader {
     readCSV(filePath: string): string [][] {
-        
+        try{
+            const content = readFileSync (filePath, 'utf8');
+            return content //use try and catch to iterate through the file
+            .trim() //remove whitespace from both ends of string
+            .split('\n') //split string object into an array
+            .map(line => line.split(',')); //create new array based on results of function
+        }
     }
 }
